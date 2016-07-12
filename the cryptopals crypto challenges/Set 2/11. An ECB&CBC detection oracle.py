b750247cc06d212307d281c078f58590686610d8
L1n <490772448@qq.com>
2016-07-12 23:42:44 +0800

    密码学实验, the cryptopals crypto challenges, 自己做的相关代码

--- the cryptopals crypto challenges/Set 2/11. An ECB&CBC detection oracle.py
+++ the cryptopals crypto challenges/Set 2/11. An ECB&CBC detection oracle.py
@@ -1,49 +1,43 @@
-'''
-Now that you have ECB and CBC working:
-
-Write a function to generate a random AES key; that's just 16 random bytes.
-
-Write a function that encrypts data under an unknown key --- that is, a function that generates a random key and encrypts under it.
-
-The function should look like:
-
-encryption_oracle(your-input)
-=> [MEANINGLESS JIBBER JABBER]
-Under the hood, have the function append 5-10 bytes (count chosen randomly) before the plaintext and 5-10 bytes after the plaintext.
-
-Now, have the function choose to encrypt under ECB 1/2 the time, and under CBC the other half (just use random IVs each time for CBC). Use rand(2) to decide which to use.
-
-Detect the block cipher mode the function is using each time. You should end up with a piece of code that, pointed at a block box that might be encrypting ECB or CBC, tells you which one is happening.
-'''
+#!/bin/env python3
 # -*- coding: utf-8 -*-
-__author__ = '__L1n__w@tch'
+# version: Python3.X
+"""
+# 题目描述
+Now that you have ECB and CBC working:
+Write a function to generate a random AES key; that's just 16 random bytes.
+Write a function that encrypts data under an unknown key --- that is, a function that generates a random key and encrypts under it.
+The function should look like:
+    encryption_oracle(your-input)
+    => [MEANINGLESS JIBBER JABBER]
+Under the hood, have the function append 5-10 bytes (count chosen randomly) before the plaintext and 5-10 bytes after the plaintext.
+Now, have the function choose to encrypt under ECB 1/2 the time, and under CBC the other half (just use random IVs each time for CBC). Use rand(2) to decide which to use.
+Detect the block cipher mode the function is using each time. You should end up with a piece of code that, pointed at a block box that might be encrypting ECB or CBC, tells you which one is happening.
 
-# 看了半天，题目要求是啥啊没太看懂，一步一步写吧
-# 到后面有点卡住，所以参考了下write_up的思路写的
-# 探测是否为ECB模式可以参考Set1的challenge8
+# 题目思路
+给定一段密文, 判断使用 ECB 模式还是使用 CBC 模式
 
+# PS
+* 到后面有点卡住，所以参考了下 write_up 的思路写的
+* 探测是否为 ECB 模式可以参考 Set1 的 challenge8
+"""
 import random
 import codecs
 from Crypto.Cipher import AES
 from itertools import zip_longest
 from collections import Counter
 
-
-def main():
-    plaintext = "plaintext" * 160
-    (mode, ciphertext) = encrypt_oracle(plaintext)
-    print("mode use:{0}, guess mode:{1}".format(mode, "ECB" if is_using_ecb(ciphertext) else "CBC"))
+__author__ = '__L1n__w@tch'
 
 
-def prf(key_size=16):
+def generate_random_bytes(key_size=16):
     """
+    产生指定数量的随机字节
     Write a function to generate a random AES key; that's just 16 random bytes.
     :param key_size: 3
     :return: b'a0D'
     """
-
-    key = b"".join([bytes([value]) for value in random.sample(range(256), key_size)])
-    return key
+    random_bytes = b"".join([bytes([value]) for value in random.sample(range(256), key_size)])
+    return random_bytes
 
 
 def encrypt_oracle(data):
@@ -53,14 +47,14 @@ def encrypt_oracle(data):
     """
     size = 16
 
-    key = prf(size)
+    key = generate_random_bytes(size)
     plaintext = codecs.encode(data, "ascii")
 
-    plaintext = prf(random.randint(5, 10)) + plaintext + prf(random.randint(5, 10))
+    plaintext = generate_random_bytes(random.randint(5, 10)) + plaintext + generate_random_bytes(random.randint(5, 10))
     plaintext = pad(plaintext, size)
     choice = random.choice([AES.MODE_CBC, AES.MODE_ECB])
     if choice == AES.MODE_CBC:
-        iv = prf(size)
+        iv = generate_random_bytes(size)
         encryptor = AES.new(key, AES.MODE_CBC, IV=iv)
     elif choice == AES.MODE_ECB:
         encryptor = AES.new(key, AES.MODE_ECB)
@@ -71,31 +65,36 @@ def encrypt_oracle(data):
     return mode[choice], ciphertext
 
 
-def pad(plaintext, size=16):
-    """input: plaintext=b"abcdefg", block_size=10
-       output: b"abcdefg\x03\x03\x03"."""
-    padding_value = size - len(plaintext) % size
-    output = plaintext + bytes([padding_value]) * padding_value
+def pad(text, size=16):
+    """
+    进行填充操作
+    :param text: b"abcdefg"
+    :param size: 10
+    :return: b"abcdefg\x03\x03\x03"
+    """
+    padding_value = size - len(text) % size
+    output = text + bytes([padding_value]) * padding_value
     return output
 
 
-def is_using_ecb(ciphertext):
+def is_using_ecb(cipher_text):
     """
-    :param ciphertext: b'a' * 2000
+    判断是否是使用 ecb 模式, 判断标准: 是否存在两个以上的相同密文(一次密码本特性)
+    :param cipher_text: b'a' * 2000
     :return: True
     """
-    blocks = divide_group(ciphertext, 16)
-
+    blocks = divide_group(cipher_text, 16)
     return Counter(blocks).most_common()[0][1] > 1
 
 
-def divide_group(data, block_size):
+def divide_group(bytes_data, size):
     """
-    :param data: b"abcdefghi"
-    :block_size: 3
+    对字节流进行分组操作
+    :param bytes_data: b"abcdefghi"
+    :param size: 3
     :return: [b"abc", b"def", b"ghi"]
     """
-    args = [iter(data)] * block_size
+    args = [iter(bytes_data)] * size
 
     blocks = []
     for block in zip_longest(*args):
@@ -105,5 +104,6 @@ def divide_group(data, block_size):
 
 
 if __name__ == "__main__":
-    main()
-
+    plaintext = "plaintext" * 160
+    (mode, cipher_text) = encrypt_oracle(plaintext)
+    print("mode use:{0}, guess mode:{1}".format(mode, "ECB" if is_using_ecb(cipher_text) else "CBC"))
