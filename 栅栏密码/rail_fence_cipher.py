1a5677584e15ea43f60ec6cc8f593207476dbb1c
L1n <490772448@qq.com>
2017-03-26 16:34:48 +0800

    新栅栏密码的代码实现

--- 栅栏密码/rail_fence_cipher.py
+++ 栅栏密码/rail_fence_cipher.py
@@ -4,6 +4,7 @@
 """ 栅栏密码的代码实现
 """
 from itertools import zip_longest
+import math
 
 __author__ = '__L1n__w@tch'
 
@@ -66,9 +67,81 @@ class RailFence:
         return blocks
 
 
+class NewRailFence:
+    """
+    同样是栅栏密码, 但是上面那个要求明文密文长度和栅栏数有联系, 于是新编一个没关联的
+    """
+
+    def __init__(self, number):
+        self.num = number  # 栅栏数
+
+    def encrypt(self, text_decrypted):
+        """
+        实现栅栏加密
+        :param text_decrypted: str(), 比如 "0123456789"
+        :return: str(), 栅栏加密后的结果, 比如栅栏数为 4 的话, 结果为 "0481592637"
+        """
+        result_str = str()
+        groups = self.divide_group(text_decrypted, self.num)
+        for i in range(self.num):
+            for each_group in groups:
+                result_str += each_group.pop(0) if each_group else ""
+        return result_str
+
+    def decrypt(self, text_encrypted):
+        """
+        实现栅栏解密
+        :param text_encrypted: str(), 比如 "0481592637"
+        :return: str(), 解密后的结果, 比如栅栏数为 4 的话, 解密结果为 "0123456789"
+        """
+        result_str = str()
+        groups = self.average_divide_group(text_encrypted, math.ceil(len(text_encrypted) / self.num))
+        for i in range(self.num):
+            for each_group in groups:
+                result_str += each_group.pop(0) if each_group else ""
+        return result_str
+
+    def average_divide_group(self, raw_data, size):
+        """
+        平均分组
+        :param raw_data: str(), 比如 "0481592637"
+        :param size: int(), 比如 3
+        :return: list(), 比如 ["048", "159", "26", "37"]
+        """
+        result_list = list()
+        raw_group_list = self.divide_group(raw_data, size)
+
+        for i in range(len(raw_group_list) - 1, 1, -1):
+            diff = len(raw_group_list[i - 1]) - len(raw_group_list[i])
+            if diff != 0 and diff != 1:
+                raw_group_list[i].insert(0, raw_group_list[i - 1].pop())
+            else:
+                break
+
+        return raw_group_list
+
+    def divide_group(self, raw_data, size):
+        return [list(each_group) for each_group in list(self.chunks(raw_data, size))]
+
+    @staticmethod
+    def chunks(a_list, n):
+        """Yield successive n-sized chunks from l."""
+        for i in range(0, len(a_list), n):
+            yield a_list[i:i + n]
+
+
 if __name__ == "__main__":
     num = 5
     rail_fence = RailFence(num)
     cipher_text = rail_fence.encrypt("a" * 5 + "b" * 5 + "c" * 5 + "d" * 5 + "e" * 5)
     plaintext = rail_fence.decrypt(cipher_text)
     print("plaintext = {0}\n{num}-cipher_text = {1}".format(plaintext, cipher_text, num=num))
+
+    num = 4
+    rf = NewRailFence(num)
+    plaintext = "0123456789"
+    cipher_text = rf.encrypt(plaintext)
+    assert cipher_text == "0481592637", "cipher_text = {}".format(cipher_text)
+
+    plaintext = rf.decrypt(cipher_text)
+    assert plaintext == "0123456789", "plaintext = {}".format(plaintext)
